a1298264bd9892c6de5b4bac740b0b7d6049e9ff
ThievishJoke <hanayokoikimo@gmail.com>
2024-09-12 02:19:11 -0400

    .gradle build

--- src/main/java/hana/hanas_blocks/enchantment/ModEnchantments.java
+++ src/main/java/hana/hanas_blocks/enchantment/ModEnchantments.java
@@ -1,42 +1,86 @@
 package hana.hanas_blocks.enchantment;
 
+
 import hana.hanas_blocks.HanasBlocks;
 import net.minecraft.component.EnchantmentEffectComponentTypes;
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.effect.AttributeEnchantmentEffect;
-import net.minecraft.enchantment.effect.EnchantmentEffectTarget;
+import net.minecraft.enchantment.EnchantmentLevelBasedValue;
+import net.minecraft.enchantment.effect.*;
+import net.minecraft.enchantment.effect.entity.DamageEntityEnchantmentEffect;
+import net.minecraft.enchantment.effect.entity.DamageItemEnchantmentEffect;
+import net.minecraft.enchantment.effect.entity.SummonEntityEnchantmentEffect;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.item.Item;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.provider.number.EnchantmentLevelLootNumberProvider;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.command.EnchantCommand;
+import net.minecraft.server.command.SummonCommand;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 
-import java.util.List;
+import java.util.Objects;
 
 public class ModEnchantments {
-    private static List<Object> Enchantments;
-    //public static final RegistryKey<Enchantment> LUCKY = Enchantment
-    //        .builder(Enchantment.definition(ItemTags.WEAPON_ENCHANTABLE), 2, 3, Enchantment.leveledCost(10,10),
-    //                Enchantment.leveledCost(25, 10), 4, AttributeModifierSlot.MAINHAND)
-    //        .addEffect(EnchantmentEffectComponentTypes.ATTRIBUTES, new AttributeEnchantmentEffect(Identifier.ofVanilla("")));
-    //public static Enchantment LUCKY =
-    /*
-    public static Enchantment LIGHTNING_STRIKER = registerEnchantment("lightning_striker",
-        new LightningStrikerEnchantment(Enchantment.builder(Enchantment.definition(Rarity.EPIC,
-            EnchantmentEffectTarget.DAMAGING_ENTITY, WEAPON_ENCHANTABLE, EquipmentSlot.MAINHAND))));
-    public static Enchantment FROST_THORN = registerEnchantment("frost_thorn",
-        new FrostThornEnchantment(Enchantment.builder(Enchantment.definition(Rarity.EPIC,
-            EnchantmentEffectTarget.ATTACKER, CHEST_ARMOR_ENCHANTABLE, EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET)));
+    public static final Enchantment.Builder FROST_THORN = new Enchantment.Builder(Enchantment.definition(
+            getOrThrow(ItemTags.CHEST_ARMOR_ENCHANTABLE),
+            1,
+            5,
+            Enchantment.constantCost(50),
+            Enchantment.constantCost(75),
+            8,
+            AttributeModifierSlot.CHEST)
+    ).addEffect(
+            EnchantmentEffectComponentTypes.POST_ATTACK,
+            EnchantmentEffectTarget.VICTIM,
+            EnchantmentEffectTarget.ATTACKER,
+            AllOfEnchantmentEffects.allOf(
+                    new DamageEntityEnchantmentEffect(
+                            EnchantmentLevelBasedValue.constant(1.0F), EnchantmentLevelBasedValue.constant(5.0F), getOrThrow(DamageTypes.THORNS)
+                    ),
+                    new DamageItemEnchantmentEffect(EnchantmentLevelBasedValue.constant(2.0F))
+            ),
+            RandomChanceLootCondition.builder(EnchantmentLevelLootNumberProvider.create(EnchantmentLevelBasedValue.linear(0.15F))))
+    .addEffect(
+            EnchantmentEffectComponentTypes.ATTRIBUTES,
+            new AttributeEnchantmentEffect(
+                    Identifier.ofVanilla("enchantment.slowness"),
+                    EntityAttributes.GENERIC_MOVEMENT_SPEED,
+                    EnchantmentLevelBasedValue.linear(1.0F),
+                    EntityAttributeModifier.Operation.ADD_VALUE
+            )
 
-    //public static Enchantment RAPID_FIRE = registerEnchantment("rapid_fire",
-
-    //        new RapidFireEnchantment(Enchantment.Rarity.VERY_RARE,
-    //        EnchantmentTarget.CROSSBOW, EquipmentSlot.MAINHAND));
-
-    private static Enchantment registerEnchantment(String name, Enchantment enchantment) {
-        return Registry.register(Registries.ENCHANTMENT, Identifier.of(HanasBlocks.MOD_ID, name), enchantment);
+    );
+    //SummonCommand.summon(Objects.requireNonNull(EntityType.AREA_EFFECT_CLOUD.spawn()))
+    private static RegistryEntry<DamageType> getOrThrow(RegistryKey<DamageType> thorns) {
+        return null;
     }
-    */
+
+    private static TagKey<Item> chestArmorEnchantable;
+
+    private static RegistryEntryList<Item> getOrThrow(TagKey<Item> chestArmorEnchantable) {
+        return null;
+    }
+
+    private static RegistryKey<Enchantment> of(String name) {
+        return RegistryKey.of(RegistryKeys.ENCHANTMENT, Identifier.of(HanasBlocks.MOD_ID, "hanas_blocks"));
+    }
+
     public static void registerModEnchantments() {
-        HanasBlocks.LOGGER.info("Registering Mod Enchantments for " + HanasBlocks.MOD_ID);
+        HanasBlocks.LOGGER.info("Registering ModEnchantments for " + HanasBlocks.MOD_ID);
     }
 }
